3a0c949019a8d85a894ee7f407b5eeb40681a0e5
layyameak <Mantananew1711@gmail.com>
2025-06-30 11:09:24 +0700

    link page

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HomeComponent,CartComponent, LoginComponent],
   // imports: [RouterOutlet, LoginComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
