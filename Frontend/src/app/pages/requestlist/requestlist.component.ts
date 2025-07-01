7991096d26e38503293943d77f3360723d463a55
layyameak <Mantananew1711@gmail.com>
2025-07-01 15:49:08 +0700

    link page sidebar

---
+++ Frontend/src/app/pages/requestlist/requestlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-requestlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './requestlist.component.html',
+  styleUrl: './requestlist.component.scss'
+})
+export class RequestlistComponent {
+
+}
