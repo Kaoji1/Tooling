c8fef9a35988a92608c2912d7aa5feb0af3fb50e
flukelnwza <tananchai1805@gmail.com>
2025-07-02 16:02:15 +0700

    Update app.routes.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { HistoryComponent } from './pages/history/history.component';
+import { RequestlistComponent } from './pages/requestlist/requestlist.component';
 
 export const routes: Routes = [
 
     {path: '',redirectTo:'home',pathMatch:'full'},
     {path: 'home', component: HomeComponent},
     {path: 'cart', component: CartComponent},
-    {path: 'history' ,component: HistoryComponent }
+    {path: 'history' ,component: HistoryComponent },
+    {path: 'requestlist' ,component: RequestlistComponent },
 
-      
+
 ];
