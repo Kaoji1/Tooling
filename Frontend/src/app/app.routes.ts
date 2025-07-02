40ca498d2f89c21aefb46ca3aef7987f1af40627
sirimongkohn191 <sirimongkhonsiri30@gmail.com>
2025-07-02 17:06:54 +0700

    add historyrequest

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { HistoryComponent } from './pages/history/history.component';
+import { HistoryRequestComponent } from './pages/history-request/history-request.component';
 
 export const routes: Routes = [
 
     {path: '',redirectTo:'home',pathMatch:'full'},
     {path: 'home', component: HomeComponent},
     {path: 'cart', component: CartComponent},
-    {path: 'history' ,component: HistoryComponent }
-
+    {path: 'history' ,component: HistoryComponent },
+    {path: 'HistoryRequest' ,component: HistoryRequestComponent }
       
 ];
