818c237552daa147efc6fe988e57b43b4265cbe5
gvorn <tananchai1805@gmail.com>
2025-07-02 16:05:09 +0700

    add purchase sidebar

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,13 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { HistoryComponent } from './pages/history/history.component';
+import { RequestlistComponent } from './pages/requestlist/requestlist.component';
 
 export const routes: Routes = [
 
-    {path: '',redirectTo:'home',pathMatch:'full'},
-    {path: 'home', component: HomeComponent},
-    {path: 'cart', component: CartComponent},
-    {path: 'history' ,component: HistoryComponent }
+    // User Group Page
+    // {path: '', redirectTo: 'home', pathMatch: 'full'},
+    // {path: 'home', component: HomeComponent},
+    // {path: 'cart', component: CartComponent},
+    // {path: 'history', component: HistoryComponent},
 
-      
+    // Purchase Group Page
+    {path: '', redirectTo: 'requestlist', pathMatch: 'full'},
+    {path: 'requestlist', component: RequestlistComponent},
+
 ];
