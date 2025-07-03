780e49ae1c07501942e13c1888eb7efa847bdf36
gvorn <tananchai1805@gmail.com>
2025-07-03 08:51:17 +0700

    modified router

--- Frontend/src/app/pages/purchase/history-request/history-request.component.ts
+++ Frontend/src/app/pages/purchase/history-request/history-request.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SidebarPurchaseComponent } from '../../components/sidebar/sidebarPurchase.component';
+import { SidebarPurchaseComponent } from '../../../components/sidebar/sidebarPurchase.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
